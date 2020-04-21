02e30dbf1a05733f5c3b50c81f4657dd127b3242
Travis CI User <travis@example.org>
2020-04-21 10:15:47 +0000

    Merge pull request #19 from karelhala/update-ddf
    Update DDF to newest version

--- js/9.js
+++ js/9.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{410:function(t,n,c){"use strict";c.r(n),n.default=function(t){var n="/",c=t.split("/");return c.shift(),"beta"===c[0]&&(c.shift(),n="/beta/"),"".concat(n).concat(c[0],"/").concat(c[1]||"")}}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{429:function(t,n,c){"use strict";c.r(n),n.default=function(t){var n="/",c=t.split("/");return c.shift(),"beta"===c[0]&&(c.shift(),n="/beta/"),"".concat(n).concat(c[0],"/").concat(c[1]||"")}}}]);
 //# sourceMappingURL=9.js.map
