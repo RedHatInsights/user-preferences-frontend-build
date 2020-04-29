81e69044d4b0111c544b7680cfd33b8b77c5bfc0
Travis CI User <travis@example.org>
2020-04-29 07:46:02 +0000

    Merge pull request #32 from RedHatInsights/master
    Update master-stable to newest master

--- js/16.js
+++ js/16.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{864:function(n,e){e["getSection should return loader 1"]='\n<Skeleton\n  isDark={false}\n  size="md"\n>\n  <div\n    className="ins-c-skeleton ins-c-skeleton__md"\n  >\n     \n  </div>\n</Skeleton>\n'}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{994:function(n,e){e["getSection should return loader 1"]='\n<Skeleton\n  isDark={false}\n  size="md"\n>\n  <div\n    className="ins-c-skeleton ins-c-skeleton__md"\n  >\n     \n  </div>\n</Skeleton>\n'}}]);
 //# sourceMappingURL=16.js.map
