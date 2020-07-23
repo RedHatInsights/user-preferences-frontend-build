6d10589821ad309909ebd59b3dc45d3f0a2fc1d9
Travis CI User <travis@example.org>
2020-07-23 14:40:02 +0000

    Fix spacing in Email preferences (#38)
    * Fix spacing in Email preferences
    
    * Add spacing constants and remove !important

--- js/18.js
+++ js/18.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{1002:function(e){e.exports=JSON.parse('[{"fields":[{"name":"unsubscribe.from-all","isGlobal":true,"label":"Unsubscribe from all","initialValue":false,"component":"descriptiveCheckbox"}]}]')}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{773:function(e){e.exports=JSON.parse('[{"fields":[{"name":"unsubscribe.from-all","isGlobal":true,"label":"Unsubscribe from all","initialValue":false,"component":"descriptiveCheckbox"}]}]')}}]);
 //# sourceMappingURL=18.js.map
