5eb713964efd826f646f7f6609d5914b85947323
Travis CI User <travis@example.org>
2021-01-13 11:55:15 +0000

    Merge pull request #50 from Hyperkid123/upgrade-webpack-5
    Update to webpack 5

--- js/23.44dfa28b83db1b67c676.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{1006:function(n,e){e["getSection should return loader 1"]='\n<Skeleton\n  isDark={false}\n  size="md"\n>\n  <div\n    className="ins-c-skeleton ins-c-skeleton__md"\n  >\n     \n  </div>\n</Skeleton>\n'}}]);
-//# sourceMappingURL=23.44dfa28b83db1b67c676.js.map
